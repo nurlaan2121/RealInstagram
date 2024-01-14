b6d2e87772652ea4228c024141c0f0bb14a35051
Nurlan <nurlanabijbillaev@gmail.com>
2024-01-14 15:26:59 +0600

    feature/addTwoIcons

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,11 @@ module com.example.realinstagram {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires javafx.media;
 
 
     opens com.example.realinstagram to javafx.fxml;
     exports com.example.realinstagram;
+    exports com.example.realinstagram.controllers;
+    opens com.example.realinstagram.controllers to javafx.fxml;
 }
